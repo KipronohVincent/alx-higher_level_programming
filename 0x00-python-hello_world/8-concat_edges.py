9b5fa991276aca52489320e128a1ac44e13c2927
KipronohVincent <kipronohvincent43@gmail.com>
2022-05-30 15:09:42 +0300

    Create a new sentence

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[38:66] + " with " + str[:6]
+print(str)
