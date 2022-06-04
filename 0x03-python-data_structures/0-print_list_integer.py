dd6bd6887730a2ea8405e5182f4f574ce9edf2da
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-04 11:25:01 +0300

    zero

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if my_list is None:
-        for list in my_list:
-            print(f"{list:d}")
+     for i in range(len(my_list)):
+         print(f"{my_list[i]:d}", end=" ")
