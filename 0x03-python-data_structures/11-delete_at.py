d4791d99d35f8fd6755398c8566d94506f6f2dee
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-04 10:48:37 +0300

    delete

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    if my_list is None:
+        return
     if idx < 0 or idx >= len(my_list):
-        return my_list
-    else:
-        my_list = my_list.remove(my_list[idx])
-        return my_list
+        return
+    del my_list[idx]
+    return my_list
