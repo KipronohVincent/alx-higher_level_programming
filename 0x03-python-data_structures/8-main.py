cef16c7ff228f59eaa96851c5d5a77aa66e2f0ae
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-04 11:13:20 +0300

    more return

--- 0x03-python-data_structures/8-main.py
+++ 0x03-python-data_structures/8-main.py
@@ -3,4 +3,4 @@ multiple_returns = __import__('8-multiple_returns').multiple_returns
 
 sentence = "At school, I learnt C!"
 length, first = multiple_returns(sentence)
-print("Length: {} - First character: {}".format(length, first))
+print("Length: {:d} - First character: {}".format(length, first))
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""
+A function that returns a tuple with the length of a string and its first character.
+"""
 def multiple_returns(sentence):
     if sentence is None:
         return None, None
     if len(sentence) == 0:
-        return None, None
-    return sentence[0], sentence[-1]
+        return 0, ""
+    return len(sentence), sentence[0]
