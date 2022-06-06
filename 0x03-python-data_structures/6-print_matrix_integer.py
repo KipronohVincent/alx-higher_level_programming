6bba1fca87bbda631ddba5c7e8691d6bc796a702
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-06 15:04:19 +0300

    matrit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for col in range(len(line)):
-            print("{:d}".format(line[col]), end="")
-            if col != len(line) - 1:
-                print(" ", end="")
+            print(
+                "{:d}".format(line[col]),
+                end="" if col == len(line) - 1 else " ")
+        print("")
