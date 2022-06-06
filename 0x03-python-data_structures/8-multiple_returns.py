1ac2f433c4f67e9fd37da855348ff22a25da53fd
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-06 15:36:29 +0300

    sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -6,6 +6,6 @@ If sentence is empty, the first character should be equal to None.
 
 def multiple_returns(sentence):
     if sentence == "":
-        return None, None
+        return (0, None)
     else:
-        return sentence[0], sentence[-1]
+        return len((sentence), sentence[0])
