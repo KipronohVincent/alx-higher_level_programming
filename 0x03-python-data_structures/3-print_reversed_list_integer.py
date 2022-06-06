1a0309e6f0eaa827866d06d2ff7855217c576cd0
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-06 14:41:34 +0300

    Riversed list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in range(my_list):
-        print("{:d}".format(i)
+    if(my_list is not None):
+        my_list.reverse()
+        for list in my_list:
+            print(f"{list:d}")
