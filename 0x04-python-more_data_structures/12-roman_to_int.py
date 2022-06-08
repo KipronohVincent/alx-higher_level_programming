3cd41f026e881054802a5e519f3b9ecb686c53cb
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-08 15:21:17 +0300

    roman convertor

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    """
-    Convert a roman numeral to an integer
-    You can assume the number will be between 1 to 3999.
-    If the roman_string is not a string or None, return 0
-    """
-    if type(roman_string) != str or roman_string is None:
+    if not roman_string or type(roman_string) != str:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    num = 0
-    for i in range(len(roman_string)):
-        if i == len(roman_string) - 1:
-            num += roman_dict[roman_string[i]]
-        elif roman_dict[roman_string[i]] >= roman_dict[roman_string[i+1]]:
-            num += roman_dict[roman_string[i]]
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_n = 0
+    for j in range(len(roman_string)):
+        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
+            roman_n += roman_d[roman_string[j]] - 2 * \
+                        roman_d[roman_string[j - 1]]
         else:
-            num -= roman_dict[roman_string[i]]
-    return num
+            roman_n += roman_d[roman_string[j]]
+    return roman_n
