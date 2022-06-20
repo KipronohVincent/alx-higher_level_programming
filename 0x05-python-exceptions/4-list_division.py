7304e1dafdb57923dbc322fd61ddaa5bced0be97
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-20 15:54:33 +0300

    division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in range(list_length):
+    for i in range(0, list_length):
         try:
             num = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
