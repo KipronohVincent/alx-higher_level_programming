b9aacea9bb8e0b0439ef99b6fcc3c06ab77e41f9
KipronohVincent <kipronohvincent43@gmail.com>
2022-07-07 11:36:18 +0300

    Updated rectangle by adding the public method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,10 @@ class Rectangle(Base):
     def area(self):
         """The area of the rectangle"""
         return self.width * self.height
+
+    def display(self):
+        """Printing the rectangle in stdout"""
+        for i in range(self.y):
+            print()
+        for i in range(self.height):
+            print(" " * self.x + "#" * self.width)
