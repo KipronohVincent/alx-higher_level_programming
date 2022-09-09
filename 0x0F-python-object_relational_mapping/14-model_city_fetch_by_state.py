1e41862edeedd63b0ebe97a4862f589e2449f6ce
KipronohVincent <kipronohvincent43@gmail.com>
2022-09-09 15:46:07 +0300

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,7 +4,7 @@ from the database hbtn_0e_14_usa"""
 
 if __name__ == "__main__":
 
-  import sys
+    import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
