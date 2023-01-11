4c3b6d8c8e1d40611d69cb14c7db4396336a8b16
Mulle21 <110990945+Mulle21@users.noreply.github.com>
2023-01-11 14:03:53 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    """
-    Convert a roman numeral to an integer
-    You can assume the number will be between 1 to 3999.
-    If the roman_string is not a string or None, return 0
-    """
-    if type(roman_string) != str or roman_string is None:
-        return 0
-    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list = 0
-    for i in range(len(roman_string)):
-        if i > 0 and roman_d[roman_string[i]] > roman_d[roman_string[i - 1]]:
-            list += roman_d[roman_string[i]] - 2 * roman_d[roman_string[i - 1]]
-        else:
-            list += roman_d[roman_string[i]]
-    return list
+        roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                            'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        item = 0
+        roman = roman_string
+        if type(roman) is not str or len(roman) is 0:
+                return 0
+        for item in range(item, len(roman)):
+                if item < len(roman) - 1\
+                   and\
+                   roman_dictionary[roman[item]] <\
+                   roman_dictionary[roman[item + 1]]:
+                    result -= roman_dictionary[roman[item]]
+                else:
+                    result += roman_dictionary[roman[item]]
+        return result
